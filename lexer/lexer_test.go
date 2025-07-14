37c085f3b758121a3fe4f614868ca1aed95f1d59
tacherasasi <tacherasasi@gmail.com>
2025-07-14 19:53:30 +0300

    Update lexer_test.go

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,14 +21,15 @@ func TestNextToken(t *testing.T) {
 	           {token.EOF, ""},
 	       }
 	       l := New(input)
-	12
+								
 	for i, tt := range tests { tok := l.NextToken()
-	if tok.Type != tt.expectedType {
-	t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-	            i, tt.expectedType, tok.Type)
-	}
-	if tok.Literal != tt.expectedLiteral {
-	t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-	}
+		if tok.Type != tt.expectedType {
+		t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+		            i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
 	}
 }
