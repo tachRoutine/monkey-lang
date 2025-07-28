46955981681708bfa4f07b93cc3b8eb3a263b298
tacherasasi <tacherasasi@gmail.com>
2025-07-28 16:32:21 +0300

    more lexer code

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/tachRoutine/monkey-lang/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -7,8 +9,9 @@ type Lexer struct {
 	ch           byte // current char under examination
 }
 
-func New(input string) *Lexer{
+func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -20,4 +23,34 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
